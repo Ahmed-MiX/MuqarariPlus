e35008055a4145006134835aeb323da781829860
Hesham <hesham-git@hesham-h.net>
2026-04-02 01:53:48 +0300

    fix: course code shouldn't be unique to allow for duplicate courses for multiple majors

--- src/main/java/com/muqarariplus/platform/entity/Course.java
+++ src/main/java/com/muqarariplus/platform/entity/Course.java
@@ -15,7 +15,6 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
     private String code;
 
     @Column(nullable = false)
